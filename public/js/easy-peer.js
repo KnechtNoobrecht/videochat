86dbf199fbe426733f480087cc824b1217a1de98
Steffen Reimann <steffenreimann2011@googlemail.com>
2022-05-08 10:41:34 +0200

    peer class erweitert

--- public/js/easy-peer.js
+++ public/js/easy-peer.js
@@ -2,8 +2,8 @@
 Steffen Reimann
 07.05.2022
 */
-const socket = io()
-const roomID = window.location.pathname.split('/').pop()
+const socket = io();
+const roomID = window.location.pathname.split('/').pop();
 
 /**
 * @var peers
@@ -14,80 +14,95 @@ const roomID = window.location.pathname.split('/').pop()
 var peers = {};
 
 class Peer {
-    /**
+	/**
     * @constructor 
     * @method init() - Init File
     * @example 
     */
-    constructor() {
-        this.id = uuid()
-        this.stream 
-        this.sid 
-        this.peer = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.l.google.com:19302' } ] })
-        peers[this.id] = this
-    }
+	constructor() {
+		this.id = uuid();
+		this.stream;
+		this.sid;
+		this.peer = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.l.google.com:19302' } ] });
+		this.remoteStream;
+		peers[this.id] = this;
+	}
 
-
-    /** 
+	/** 
     * @method init
+    * @param {Object} data - initiator , socketid
     * @description Try To connect to socketid 
     * @returns {Promise}  Returns a Promise
     * @example 
     * @example 
     */
-	async init(initiator, socketid) {
+
+	async init(data) {
 		return new Promise(async (resolve, reject) => {
+			var initiator = data.initiator || false;
+			var socketid = data.socketid || false;
 
-			
-            if(this.stream) {
-                for(const track of stream.getTracks()) {
-                    this.peer.addTrack(track, stream)
-                }
-            } else {
-                dataChannel = this.peer.createDataChannel('foo')	// dummy channel to trigger ICE		
-            }
+			if (initiator) {
+				this.dataChannel = this.peer.createDataChannel('data');
 
-            const offer = await peerConnection.createOffer()
+				for (const track of stream.getTracks()) {
+					this.peer.addTrack(track, stream);
+				}
 
-            await this.peer.setLocalDescription(offer)
+				const offer = await this.peer.createOffer();
 
-            socket.emit('makePeerOfferToID', { offer: offer, initiatorsid: this.sid, connectionID: this.id })
+				await this.peer.setLocalDescription(offer);
 
-            socket.on('sendPeerAnswerToID-' + this.id, (answer) => {
-                const remoteDesc = new RTCSessionDescription(answer)
-                this.peer.setRemoteDescription(remoteDesc)
-            });
+				socket.emit('makePeerOfferToID', { offer: offer, initiatorsid: this.sid, connectionID: this.id });
 
-
+				socket.on('peerAnswer-' + this.id, (answer) => {
+					this.peer.setRemoteDescription(new RTCSessionDescription(answer));
+				});
+			} else {
+				this.peer.setRemoteDescription(new RTCSessionDescription(data.offer));
+				const answer = await this.peer.createAnswer();
+				await this.peer.setLocalDescription(answer);
+				resolve(answer);
+			}
 		});
 	}
 
+	initevents() {
+		this.peer.ontrack = async (event) => {
+			const [ remoteStream ] = event.streams;
+			this.remoteStream = remoteStream;
+		};
+		this.peer.ondatachannel = (event) => {
+			this.dataChannel = event.channel;
+		};
+		this.dataChannel.onmessage = (event) => {
+			console.log('DATA CHANNEL MESSAGE:', event.data);
+		};
+	}
+
+	sendData(data) {
+		this.dataChannel.send(data);
+	}
 }
 
-class LocalStream {
-
-}
-
-
+class LocalStream {}
 
 // Helper Functions
 
 function uuid() {
-    function ff(s) {
-        var pt = (Math.random().toString(16)+"000000000").substr(2,8);
-        return s ? "-" + pt.substr(0,4) + "-" + pt.substr(4,4) : pt ;
-    }
-    return ff() + ff(true) + ff(true) + ff();
+	function ff(s) {
+		var pt = (Math.random().toString(16) + '000000000').substr(2, 8);
+		return s ? '-' + pt.substr(0, 4) + '-' + pt.substr(4, 4) : pt;
+	}
+	return ff() + ff(true) + ff(true) + ff();
 }
 
 //listen for incoming peer offers
-socket.on('incomingPeerOffer', async (data) => {
-	console.log('incoming Peer offer')
-	
-	peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer))
-	
-	const answer = await peerConnection.createAnswer()
-	await peerConnection.setLocalDescription(answer)
+socket.on('peerOffer', async (data) => {
+	console.log('incoming Peer offer');
 
-	socket.emit('sendPeerAnswerToID', { answer: answer, offerer: data.offerer })
-})
+	// { offer: offer, initiatorsid: this.sid, connectionID: this.id }
+	const peer = new Peer();
+	var p = peer.init();
+	socket.emit('peerAnswer', { answer: p, initiatorsid: data.initiatorsid });
+});
--- server.js
+++ server.js
@@ -1,64 +1,68 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
-const { v4: uuidv4 } = require('uuid')
-const PORT = 80
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
+const PORT = 80;
 
 io.on('connection', (socket) => {
-  socket.emit('ID', socket.id)
+	socket.emit('ID', socket.id);
 
-  socket.on('joinRoom', (roomID) => {
-    socket.join(roomID)
-    console.log("Subscribed",socket.id,"to room", roomID)
-  })
+	socket.on('joinRoom', (roomID) => {
+		socket.join(roomID);
+		console.log('Subscribed', socket.id, 'to room', roomID);
+	});
 
-  socket.on('makePeerOffer', data => {
-    console.log('Peer offer made by', socket.id)
-    socket.to(data.roomID).emit('incomingPeerOffer', {offer: data.offer, offerer: socket.id})
-  })  
-  
-  socket.on('makePeerOfferToID', data => {
-    // data = { offer: offer, initiatorsid: this.sid, connectionID: this.id }
-    console.log('Peer offer made by', socket.initiatorsid)
-    io.to(data.initiatorsid).emit('sendPeerAnswerToID' + socket.initiatorsid, data)
-  })
+	socket.on('makePeerOffer', (data) => {
+		console.log('Peer offer made by', socket.id);
+		socket.to(data.roomID).emit('incomingPeerOffer', { offer: data.offer, offerer: socket.id });
+	});
 
+	socket.on('makePeerAnswer', (data) => {
+		console.log(data);
+		console.log('Peer answer made by', socket.id);
+		io.to(data.offerer).emit('incomingPeerAnswer', data.answer);
+	});
 
-  socket.on('makePeerAnswer', data => {
-    console.log(data);
-    console.log('Peer answer made by', socket.id);
-    io.to(data.offerer).emit('incomingPeerAnswer', data.answer)
-  })
+	socket.on('new-ice-candidate', (data) => {
+		console.log('ICE CANDIDATE');
+		socket.to(data.roomID).emit('incomingICEcandidate', data.candidate); // wurde von standard-emit zu dem hier geändert, jetzt kein fehler mehr aber ?
+	});
 
-  socket.on('new-ice-candidate', data => {
-    console.log("ICE CANDIDATE");
-    socket.to(data.roomID).emit('incomingICEcandidate',data.candidate) // wurde von standard-emit zu dem hier geändert, jetzt kein fehler mehr aber ? 
-  })
-})
+	socket.on('peerOffer', (data) => {
+		// data = { offer: offer, initiatorsid: this.sid, connectionID: this.id }
+		console.log('Peer offer made by', socket.initiatorsid);
+		io.to(data.initiatorsid).emit('peerOffer' + socket.initiatorsid, data);
+	});
+
+	socket.on('peerAnswer', (data) => {
+		// data = { offer: offer, initiatorsid: this.sid, connectionID: this.id }
+		console.log('Peer offer made by', socket.initiatorsid);
+		io.to(data.initiatorsid).emit('peerAnswer-' + socket.initiatorsid, data);
+	});
+});
 
 io.on('joinRoom', (socket, roomID) => {
-  console.log(socket, roomID)
-})
+	console.log(socket, roomID);
+});
 
 //use public folder
-app.use(express.static(__dirname + "/public/"))
+app.use(express.static(__dirname + '/public/'));
 
-app.get('/', function (req, res) {
-  res.redirect('/rooms/' + uuidv4())
-})
+app.get('/', function(req, res) {
+	res.redirect('/rooms/' + uuidv4());
+});
 
-app.get('/rooms/:id', function (req, res) {
-    res.sendFile(path.join(__dirname + "/public/video.html"))
-})
+app.get('/rooms/:id', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/video.html'));
+});
 
 // reference test
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname + "/public/reference.html"))
-})
+app.get('/test', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/reference.html'));
+});
 
 server.listen(PORT, () => {
-    console.log('Server started on port '+PORT)
-})
-
+	console.log('Server started on port ' + PORT);
+});
